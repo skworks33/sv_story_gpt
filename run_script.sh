5d491030257b59ace7710a42e87533d4b2daf989
skworks <47235313+skworks33@users.noreply.github.com>
2023-04-23 15:48:10 +0900

    Create run_script.sh

---
+++ run_script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export OPENAI_API_KEY=your_openai_api_key
+export SLACK_WEBHOOK_URL=https://hooks.slack.com/services/your_slack_webhook_url
+/path/to/python3 /path/to/sv_story_gpt/chatgpt_to_slack.py
